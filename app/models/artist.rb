c4ca48aada3d7852cb0239a6a3670733824f1b6f
Jeff Swisher <jefftswisher@gmail.com>
2020-01-20 20:34:09 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -14,7 +14,7 @@ class Artist < ActiveRecord::Base
 
   def genre_count
     #return the number of genres associated with the artist
-    self.genres.count
+    self.songs.genres.count
   end
 end
 

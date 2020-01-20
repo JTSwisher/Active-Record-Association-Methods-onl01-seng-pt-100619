3732601089c69845180d137b852e48fe3379e16a
Jeff Swisher <jefftswisher@gmail.com>
2020-01-20 20:53:33 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,9 +14,12 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    
     #self.artists.map(&:name)
+    
     self.artists.map do |n|
       n.name 
     end 
   end
+  
 end
